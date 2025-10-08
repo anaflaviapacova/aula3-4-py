8d2ee8b2c6fc8aa69f91245d0b0dcd5f27ca6750
anaflaviapacova <anaflaviapacova@colegiopaulodetarso.com.br>
2025-10-08 15:56:24 +0000

    py

---
+++ conjuntos.py
@@ -0,0 +1,14 @@
+1. Lista de compras 
+compras = ["maçã", "banana", "laranja", "pão", "leite"]
+2. Coordenadas geográficas (x, y)
+coordenadas_brasilia = (-15.793889, -47.882778)
+3. CPFs de clientes 
+cpfs = {"123.456.789-00", "987.654.321-00", "111.222.333-44"}
+4. Notas dos alunos 
+notas = [8.5, 7.0, 9.5, 6.0, 10.0]
+5. Tarefas
+tarefas = {"estudar", "fazer exercícios", "ler livro", "assistir aula"}
+6. Meses do ano
+meses = ("setembro", "outubro", "novembro", "dezembro")
+7. Primeiro nome dos alunos
+nomes = ["Ana", "Lucas", "Bruna", "Maria"]
